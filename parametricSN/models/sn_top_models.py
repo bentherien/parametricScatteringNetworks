9ad324c99655366bb8d64296aa73e188426ccdba
Benjamin Therien <btherien@v1040-wn-rt-b-98-220.campus-dynamic.uwaterloo.ca>
2022-03-22 14:17:56 -0400

    WRN-16-8 for computational and memory complexity expriments

--- parametricSN/models/sn_top_models.py
+++ parametricSN/models/sn_top_models.py
@@ -54,6 +54,13 @@ def conv3x3(in_planes, out_planes, stride=1):
                      padding=1, bias=False)
 
 
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
+                    bias=False)
+
+
+
 class BasicBlock(nn.Module):
     """
     Standard wideresnet basicblock
@@ -194,3 +201,92 @@ class sn_Resnet50(nn.Module):
         x = self.model_ft(x)
         return x
 
+
+
+class BasicBlockWRN16_8(nn.Module):
+    def __init__(self, inplanes, planes, dropout, stride=1):
+        super(BasicBlockWRN16_8, self).__init__()
+        self.bn1 = nn.BatchNorm2d(inplanes)
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.dropout = nn.Dropout(dropout)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes)
+        self.relu = nn.ReLU(inplace=True)
+        if stride != 1 or inplanes != planes:
+            self.shortcut = conv1x1(inplanes, planes, stride)
+            self.use_conv1x1 = True
+        else:
+            self.use_conv1x1 = False
+
+    def forward(self, x):
+        out = self.bn1(x)
+        out = self.relu(out)
+
+        if self.use_conv1x1:
+            shortcut = self.shortcut(out)
+        else:
+            shortcut = x
+
+        out = self.conv1(out)
+
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.conv2(out)
+
+        out += shortcut
+
+        return out
+
+
+
+class WideResNet16_8(nn.Module):
+    """WRN-16-8 from https://arxiv.org/abs/1605.07146"""
+    def __init__(self, depth, width, num_classes=10, dropout=0.3):
+        super(WideResNet16_8, self).__init__()
+
+        layer = (depth - 4) // 6
+
+        self.inplanes = 16
+        self.conv = conv3x3(3, 16)
+        self.layer1 = self._make_layer(16*width, layer, dropout)
+        self.layer2 = self._make_layer(32*width, layer, dropout, stride=2)
+        self.layer3 = self._make_layer(64*width, layer, dropout, stride=2)
+        self.bn = nn.BatchNorm2d(64*width)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64*width, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data = nn.init.kaiming_normal_(m.weight.data, mode='fan_out', nonlinearity='relu')
+
+    def _make_layer(self, planes, blocks, dropout, stride=1):
+        layers = []
+        for i in range(blocks):
+            layers.append(BasicBlockWRN16_8(self.inplanes, planes, dropout, stride if i == 0 else 1))
+            self.inplanes = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+        
+    def countLearnableParams(self):
+        """returns the amount of learnable parameters in this model"""
+        count = 0
+        for t in self.parameters():
+            count += t.numel()
+        return count
